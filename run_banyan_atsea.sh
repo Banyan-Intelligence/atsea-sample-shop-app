c9641eec2f102729d16bfdd0b6a7edb6a023265e
bishalj054bi <bishal@banyanintelligence.com>
2024-01-11 17:05:16 +0530

    minor change

--- run_banyan_atsea.sh
+++ run_banyan_atsea.sh
@@ -2,6 +2,6 @@ mkdir banyan_atsea
 cd banyan_atsea
 git clone -b dev https://github.com/Banyan-Intelligence/atsea-sample-shop-app.git
 cd atsea-sample-shop-app
-sudo cat prmetheus_config.txt >> /etc/prometheus/prometheus.yml
+sudo cat prometheus_config.txt >> /etc/prometheus/prometheus.yml
 sudo systemctl restart prometheus.service
 sudo docker compose up --build
